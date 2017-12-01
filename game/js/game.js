b91862c44eb990fff83d4c92711ae605dffdef3b
quyihang <1051216331@qq.com>
2017-12-01 15:15:35 +0800

    '11'

--- game/js/game.js
+++ game/js/game.js
@@ -11,17 +11,17 @@ var city = 'beijing';
 var pass_count = 0;
 var finished = false;
 var question_index = 1;
-function load(){
+function load() {
     var url;
-    if(city == 'shanghai'){
+    if (city == 'shanghai') {
         url = '/streetgame/backstage/load.php?city_index=0';
-    }else if(city == 'beijing') {
+    } else if (city == 'beijing') {
         url = '/streetgame/backstage/load.php?city_index=1';
     }
-    pic_arr = load_request(url,1000, update_pic);
-    document.addEventListener('touchstart',touch,false);
-    document.addEventListener('touchmove',touch,false);
-    document.addEventListener('touchend',touch,false);
+    pic_arr = load_request(url, 1000, update_pic);
+    document.addEventListener('touchstart', touch, false);
+    document.addEventListener('touchmove', touch, false);
+    document.addEventListener('touchend', touch, false);
     var isdown = false;
     var angle = 0;
     var startangle = 0;
@@ -31,11 +31,11 @@ function load(){
     var istaped = false;
     var istaped_pic = false;
     var ismoved = false;
-    function touch (event){
+    function touch(event) {
         var event = event || window.event;
 
         // var plate = document.getElementById("testcss");
-        switch(event.type){
+        switch (event.type) {
             case "touchstart":
 
                 var ox = parseInt(event.touches[0].clientX) - parseInt(plateX);//计算出鼠标相对于画布中心的位置
@@ -43,89 +43,89 @@ function load(){
                 var to = Math.abs(ox / oy);
 
                 startangle = Math.atan(to) / (2 * Math.PI) * 360;//鼠标相对于旋转中心的角度
-                if (ox < 0 && oy < 0)
-                {
+                if (ox < 0 && oy < 0) {
                     startangle = -startangle;
-                } else if (ox < 0 && oy > 0)
-                {
+                } else if (ox < 0 && oy > 0) {
                     startangle = -(180 - startangle)
-                } else if (ox > 0 && oy < 0)
-                {
+                } else if (ox > 0 && oy < 0) {
                     startangle = startangle;
-                } else if (ox > 0 && oy > 0)
-                {
+                } else if (ox > 0 && oy > 0) {
                     startangle = 180 - startangle;
                 }
                 startangle -= oldangle;
-                if(ox<125&&ox>-125&&oy<125&&oy>-125){
+                if (ox < 125 && ox > -125 && oy < 125 && oy > -125) {
                     isdown = true;
                 }
-                if(ox<50&&ox>-50&&oy<50&&oy>-50){
+                if (ox < 50 && ox > -50 && oy < 50 && oy > -50) {
                     istaped = true;
                 }
-                if(event.touches[0].clientY<document.getElementById('imgdiv').offsetHeight){
+                if (event.touches[0].clientY < document.getElementById('imgdiv').offsetHeight) {
                     istaped_pic = true;
                 }
                 break;
             case "touchend":
                 oldangle = angle;
-                while(oldangle<0||oldangle>=360){
-                    if(oldangle<0){
-                        oldangle+=360;
-                    }else{
-                        oldangle-=360;
+                while (oldangle < 0 || oldangle >= 360) {
+                    if (oldangle < 0) {
+                        oldangle += 360;
+                    } else {
+                        oldangle -= 360;
                     }
                 }
-                if(istaped==true&&ismoved==false){   // 模拟tap事件
-                    if(pic_index >= pic_index_max){ // 游戏做完
+                if (istaped == true && ismoved == false) {   // 模拟tap事件
+                    if (pic_index >= pic_index_max) { // 游戏做完
                         finished = true;
                         send_score(edit_title);
                         show_pane_final();
                         // document.getElementById('score').innerHTML = '总得分：'+totalgoal;
-                    }else{
+                    } else {
                         var realAngle = Number(global_pic_arr[pic_index]['jpg_direction']);
-                        console.log(pic_index+','+global_pic_arr[pic_index]['jpg_name']+','+global_pic_arr[pic_index]['jpg_direction']);
+                        console.log(pic_index + ',' + global_pic_arr[pic_index]['jpg_name'] + ',' + global_pic_arr[pic_index]['jpg_direction']);
                         oldangle = parseInt(oldangle);
-                        var diff = Math.abs(oldangle-realAngle);
+                        var diff = Math.abs(oldangle - realAngle);
                         var goal = 0;
-                        if(diff>180){
+                        if (diff > 180) {
                             diff = 360 - diff;
                         }
-                        if(diff<=30){
+                        if (diff <= 30) {
                             goal = 10;
-                        }else if(diff<90){
-                            goal = parseInt(-(diff-90)/6)+1;
-                        }else{
+                        } else if (diff < 90) {
+                            goal = parseInt(-(diff - 90) / 6) + 1;
+                        } else {
                             goal = 0;
                         }
                         totalgoal += goal;
                         pic_index += 1;
-                        document.getElementById('total_score').innerHTML = '得分：'+totalgoal;
-                        if(pic_index<pic_index_max){
+                        document.getElementById('total_score').innerHTML = '得分：' + totalgoal;
+                        if (pic_index < pic_index_max) {
                             update_pic(global_pic_arr, pic_index);
-                            question_index+=1;
-                            document.getElementById('question_index').innerHTML = '题目：'+question_index+'/10';
-                            $(".circle").each(function(){
+                            question_index += 1;
+                            document.getElementById('question_index').innerHTML = '题目：' + question_index + '/10';
+                            $(".circle").each(function () {
                                 $(this).removeClass("checked");
                             });
-                            $(".circle:eq("+(question_index-1)+")").addClass("checked");
+                            $(".circle:eq(" + (question_index - 1) + ")").addClass("checked");
+                        } else {
+                            finished = true;
+                            send_score(edit_title);
+                            show_pane_final();
                         }
                     }
-                    if(pic_index == 2){
+                    if (pic_index == 2) {
                         get_ip();
                     }
-                    if(pic_index == 5){
+                    if (pic_index == 5) {
                         console.log(ip_local);
                     }
                 }
-                if(istaped_pic==true && finished == false){
-                    if(pass_count<3){
+                if (istaped_pic == true && finished == false) {
+                    if (pass_count < 3) {
                         pic_index += 1;
                         update_pic(global_pic_arr, pic_index);
                         pic_index_max += 1;
                         pass_count += 1;
                         istaped_pic = false;
-                        document.getElementById('score').innerHTML = '您还有'+(3-pass_count)+'次跳过机会';
+                        document.getElementById('score').innerHTML = '您还有' + (3 - pass_count) + '次跳过机会';
                     }
                 }
                 ismoved = false;
@@ -137,27 +137,23 @@ function load(){
                 event.stopPropagation();
                 ismoved = true;
                 // angle = (event.touches[0].clientY - plateY) / (event.touches[0].clientX - plateX)
-                if(isdown){
+                if (isdown) {
                     var ox = parseInt(event.touches[0].clientX) - parseInt(plateX);//计算出鼠标相对于画布中心的位置
                     var oy = parseInt(event.touches[0].clientY) - parseInt(plateY);
                     var to = Math.abs(ox / oy);
 
                     angle = Math.atan(to) / (2 * Math.PI) * 360;//鼠标相对于旋转中心的角度
-                    if (ox < 0 && oy < 0)
-                    {
+                    if (ox < 0 && oy < 0) {
                         angle = -angle;
-                    } else if (ox < 0 && oy > 0)
-                    {
+                    } else if (ox < 0 && oy > 0) {
                         angle = -(180 - angle)
-                    } else if (ox > 0 && oy < 0)
-                    {
+                    } else if (ox > 0 && oy < 0) {
                         angle = angle;
-                    } else if (ox > 0 && oy > 0)
-                    {
+                    } else if (ox > 0 && oy > 0) {
                         angle = 180 - angle;
                     }
                     angle -= startangle;
-                    $("#testcss").css("transform","rotate("+angle+"deg)");
+                    $("#testcss").css("transform", "rotate(" + angle + "deg)");
                 }
                 break;
         }
@@ -165,63 +161,63 @@ function load(){
 
     }
 
-    function load_request( url, time, callback){
+    function load_request(url, time, callback) {
         var pic_arr = [];
         var request = new XMLHttpRequest();
         var timeout = false;
-        var timer = setTimeout( function(){
+        var timer = setTimeout(function () {
             timeout = true;
             request.abort();
-        }, time );
-        request.open( "GET", url );
-        request.onreadystatechange = function(){
-            if( request.readyState !== 4 ) return;
-            if( timeout ) return;
-            clearTimeout( timer );
-            if( request.status === 200 ){
+        }, time);
+        request.open("GET", url);
+        request.onreadystatechange = function () {
+            if (request.readyState !== 4) return;
+            if (timeout) return;
+            clearTimeout(timer);
+            if (request.status === 200) {
                 pic_arr = eval('(' + request.response + ')');
                 callback(pic_arr);
             }
         }
-        request.send( null );
+        request.send(null);
         return pic_arr;
     }
 
-    function update_pic(pic_arr){
-        var i = arguments[1]?arguments[1]:0;
-        if(city == 'shanghai'){
-            document.getElementById("streetimg").src = "http://120.26.51.49/data/streetgame/shanghai_outring_50m/"+pic_arr[i]['jpg_name']+".jpg";
-        }else if(city == 'beijing'){
-            document.getElementById("streetimg").src = "http://120.26.51.49/data/streetgame/beijing_5ring_50m/"+pic_arr[i]['jpg_name']+".jpg";
+    function update_pic(pic_arr) {
+        var i = arguments[1] ? arguments[1] : 0;
+        if (city == 'shanghai') {
+            document.getElementById("streetimg").src = "http://120.26.51.49/data/streetgame/shanghai_outring_50m/" + pic_arr[i]['jpg_name'] + ".jpg";
+        } else if (city == 'beijing') {
+            document.getElementById("streetimg").src = "http://120.26.51.49/data/streetgame/beijing_5ring_50m/" + pic_arr[i]['jpg_name'] + ".jpg";
         }
-        if(i<=1){
+        if (i <= 1) {
             console.log(pic_arr);
             global_pic_arr = pic_arr;
         }
     }
 
-    function get_ip(){
+    function get_ip() {
         var request = new XMLHttpRequest();
         // request.open("GET", "http://jsonip.com/?callback=?");
         request.open("GET", "/streetgame/backstage/load.php?city_index=1/");
-        request.onreadystatechange = function(){
-            if(request.status == 200){
+        request.onreadystatechange = function () {
+            if (request.status == 200) {
                 console.log(request.response);
                 ip_local = eval('(' + request.response + ')')[0];
             }
         }
     }
 
-    function send_score(callback){
+    function send_score(callback) {
         var commit_url = "/streetgame/backstage/commit.php";
-        $.post(commit_url,{ip:ip_local,score:totalgoal})
+        $.post(commit_url, { ip: ip_local, score: totalgoal })
             .done(function (data) {
                 callback();
             });
     }
 
-    function edit_title(){
-        document.title = '方向感测试小游戏\n您的总得分：'+totalgoal;
+    function edit_title() {
+        document.title = '方向感测试小游戏\n您的总得分：' + totalgoal;
     }
 }
 
@@ -242,7 +238,7 @@ $(document).ready(function () {
 })
 
 
-var randomScalingFactor = function() {
+var randomScalingFactor = function () {
     return Math.round(Math.random() * 100);
 };
 
@@ -258,10 +254,10 @@ function show_pane_final() {
             this.fillStyle = 'rgb(156,190,183)';  //填充色
             this.lineCap = 'round';
         }
-         
-        Circle.prototype.draw = function(ctx) {
+
+        Circle.prototype.draw = function (ctx) {
             ctx.beginPath();
-            ctx.arc(100, 100, this.radius, 0, Math.PI*2, true);  // 坐标为250的圆，这里起始角度是0，结束角度是Math.PI*2
+            ctx.arc(100, 100, this.radius, 0, Math.PI * 2, true);  // 坐标为250的圆，这里起始角度是0，结束角度是Math.PI*2
             ctx.lineWidth = this.lineWidth;
             ctx.strokeStyle = this.strokeStyle;
             ctx.stroke();  // 这里用stroke画一个空心圆，想填充颜色的童鞋可以用fill方法
@@ -269,35 +265,35 @@ function show_pane_final() {
 
         function Ring(startAngle, percent) {
             Circle.call(this);
-            this.startAngle = startAngle || 3*Math.PI/2; //弧起始角度
+            this.startAngle = startAngle || 3 * Math.PI / 2; //弧起始角度
             this.percent = percent;  //弧占的比例
         }
-         
+
         Ring.prototype = Object.create(Circle.prototype);
-         
-        Ring.prototype.drawRing = function(ctx) {
-             this.draw(ctx);
-             ctx.beginPath();
-             var anglePerSec = 2 * Math.PI / (100 / this.percent); // 蓝色的弧度
-             ctx.arc(100, 100, this.radius, this.startAngle, this.startAngle + anglePerSec, false); //这里的圆心坐标要和cirle的保持一致
-             ctx.strokeStyle = this.fillStyle;
-             ctx.lineCap = this.lineCap;
-             ctx.stroke();
-             ctx.closePath();
+
+        Ring.prototype.drawRing = function (ctx) {
+            this.draw(ctx);
+            ctx.beginPath();
+            var anglePerSec = 2 * Math.PI / (100 / this.percent); // 蓝色的弧度
+            ctx.arc(100, 100, this.radius, this.startAngle, this.startAngle + anglePerSec, false); //这里的圆心坐标要和cirle的保持一致
+            ctx.strokeStyle = this.fillStyle;
+            ctx.lineCap = this.lineCap;
+            ctx.stroke();
+            ctx.closePath();
         }
 
         $("#score-donut").text(totalgoal);
-        if(totalgoal>60){
+        if (totalgoal > 60) {
             $("#score-rank").text("男友力max的活地图！");
-        }else if (totalgoal>35){
+        } else if (totalgoal > 35) {
             $("#score-rank").text("您的方向感超级好！赞！");
-        }else if (totalgoal>15){
+        } else if (totalgoal > 15) {
             $("#score-rank").text("亲，出门记得看地图哦！");
-        }else {
+        } else {
             $("#score-rank").text("呃，I'm Sorry...");
         }
         var canvas = document.getElementById('canvas');
-        var ctx =  canvas.getContext('2d');
+        var ctx = canvas.getContext('2d');
         var ring = new Ring(0, totalgoal);  // 从2*Math.PI/3弧度开始，进度为50%的环
         ring.drawRing(ctx);
     });
