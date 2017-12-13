49eaa88f323e6fe5cff31f0aebd981637af1dfcf
瞿一航 <quyihang@quyihangs-MacBook.local>
2017-12-13 23:56:48 +0800

    11

--- backstage/wx/signature.php
+++ backstage/wx/signature.php
@@ -3,5 +3,5 @@ require_once "jssdk.php";
 $url = $_GET["url"];
 $jssdk = new JSSDK("wx3346fcca380ae7ec", "4be4564a20af7091496f352a97166386", $url);
 $signPackage = $jssdk->GetSignPackage();
-echo $signPackage;
+echo json_encode($signPackage);
 ?>
